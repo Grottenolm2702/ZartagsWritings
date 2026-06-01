35fdb7ca1dc9961bdaee0875dcc97d14478741d4
Grottenolm2702 <wronny809@gmail.com>
2026-06-01 08:05:57 +0200

    chore: import missing styles and legacy script for original styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from "./App";
 import "./styles/base.css";
 import "./styles/navigation.css";
 import "./styles/content.css";
+import "./styles/form.css";
+import "./styles/about.css";
+import "./styles/overview.css";
+import "./script.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
