5d6961e47593531432b887d3ed5683aa35262aee
Grottenolm2702 <wronny809@gmail.com>
2026-06-01 08:14:48 +0200

    fix(nav): ensure nav above overlay (z-index) and use react-router Link for nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
@@ -6,18 +7,18 @@ export default function Header() {
       <nav id="navbar">
         <ul>
           <li className="home-li">
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <a className="accent-link" href="/login">
+            <Link className="accent-link" to="/login">
               Login
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
     </header>
-  );
+  )
 }
